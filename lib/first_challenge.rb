27f6e6d95ddf7b1a609c49c5c9e4b8c528afaaef
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 14:05:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,9 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"][:favorite_icecream_flavors]
+contacts.each do |people, description|
+  
+end
 
   #remember to return your newly altered contacts hash!
   contacts
